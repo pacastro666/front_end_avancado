e391a794b98c90e91bf67a02de7a87789366ad20
pacastro666@gmail.com <pauloantunes66@hotmail.com>
2024-07-11 14:52:04 -0300

    acessando um produto especifico

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home"
 import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
 function App() {
 
 return (
@@ -9,6 +10,7 @@ return (
         <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/products' element={<Products/>}></Route>
+        <Route path='/products/:id/:title' element={<ProductDetails/>}></Route>
         </Routes>    
     </BrowserRouter>
    
---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,34 @@
+import { Grid } from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom"
+import NavBar from "../components/navbar/Header";
+import Footer from "../components/footer/Footer";
+
+
+export default function ProductDetails() {
+  let { state } = useLocation();
+  const navigate = useNavigate();
+
+  return (
+   
+    <div className="detailed-content">
+         <NavBar/>            
+      <div>
+        <img width={500} src={state.productInfo.image} alt='Imagem do produto'/>
+      </div>
+      <div>
+        <h2>{state.productInfo.title}</h2>
+        <p>{state.productInfo.description}</p>
+        {/* https://mui.com/material-ui/react-grid/ */}
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          sx={{border: '1px solid red', height: 200}}>
+            <Footer/> 
+        </Grid>
+        
+      </div>
+    </div>
+  )
+}
