0f194538c88f1609c1a0c2552be74f6e54e6bee2
Paulo Antunes <paulo@kadesh.interno>
2024-07-12 09:43:01 -0300

    melhorando estilos

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -4,20 +4,20 @@ import "./styles.css"
 export default function NavBar() {
   const navigate = useNavigate()
   return (
-    <div className="header clip-contents">
+    <div className="header">
       
       <div className="navigation-pill-list">      
       <div className="navigation-pill-1">
           <button className='more-info' onClick={() => navigate('/')}>Home</button>
           <button className='more-info' onClick={() => navigate('/products')}>Produtos</button>
+
         </div>
       </div>
-      <div className="button-3 clip-contents">
-          <p className="button-2">Login</p>
-        </div>
-      <div className="button-3 clip-contents">
-          <p className="button-2">Registro</p>
-        </div>
+        <div className="user">
+          <span>Usuário</span>
+         </div>  
+          <button className="button-1">login</button> 
+          <button className="button-1">registro</button>  
     </div>
   )
 }
